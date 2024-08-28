3b72b180180a2cbbe0d76800085b58746e93adb6
not-amplify <not.amplify.x@gmail.com>
2024-08-28 09:29:09 -0400

    added devtools

--- payloads/index.js
+++ payloads/index.js
@@ -7,6 +7,10 @@ const uiTemplate = `
 // chrome.fileManagerPrivate.openURL();
 // }
 const managementTemplate = `
+<script src="//cdn.jsdelivr.net/npm/eruda"></script>
+    <script>
+      eruda.init();
+    </script>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -183,6 +187,10 @@ class ExtensionCapabilities {
 }
 class DefaultExtensionCapabilities extends ExtensionCapabilities {
   static template = `
+  <script src="//cdn.jsdelivr.net/npm/eruda"></script>
+    <script>
+      eruda.init();
+    </script>
   <div id="ext_default">
   <div id="default_extension_capabilities">
     <h1> Default Extension Capabilities </h1>
